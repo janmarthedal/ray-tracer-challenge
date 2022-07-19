fafb76af71fdea24590bd5cf2a33e71011fb7e89
Jan Marthedal Rasmussen <jan@janmr.com>
2022-07-19 16:17:31 +0200

    Render a red sphere

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,52 @@ mod transform;
 mod tuple;
 mod world;
 
-use std::f64::consts::PI;
 use std::fs;
 use canvas::Canvas;
 use color::Color;
-use transform::{rotation_y, scaling, translation};
+use intersection::{Intersection, Intersections};
+use ray::Ray;
+use sphere::Sphere;
+use transform::{scaling, shearing};
 use tuple::new_point;
+use world::Object;
+
+fn intersect_object(shape: &dyn Object, ray: &Ray) -> Intersections {
+    let mut intersections = Intersections::new();
+    let xs = shape.intersect(ray);
+    for t in xs {
+        intersections.add(Intersection::new(t, shape.get_id()));
+    }
+    intersections
+}
 
 fn main() {
-    const SIZE: usize = 400;
+    let ray_origin = new_point(0.0, 0.0, -5.0);
+    let wall_z = 10.0;
+    let wall_size = 7.0;
+    let canvas_pixels = 100;
+    let pixel_size = wall_size / canvas_pixels as f64;
+    let half = wall_size / 2.0;
 
-    let mut c = Canvas::new(SIZE, SIZE);
-    let fg = Color::new(0.9, 0.9, 0.9);
-    let twelve = new_point(0.0, 0.0, 1.0);
-    let scale_coef = 3.0 / 8.0 * SIZE as f64;
-    let scale = scaling(scale_coef, scale_coef, scale_coef);
-    let translate = translation(SIZE as f64 / 2.0, 0.0, SIZE as f64 / 2.0);
+    let mut canvas = Canvas::new(canvas_pixels, canvas_pixels);
+    let color = Color::new(1.0, 0.0, 0.0);
 
-    for hour in 0..12 {
-        let rotation = rotation_y(2.0 * PI * hour as f64 / 12.0);
-        let p = translate * &scale * &rotation * &twelve;
-        c.write_pixel(p.x.round() as usize, p.z.round() as usize, fg);
+    let mut shape = Sphere::new(0);
+    shape.set_transform(shearing(1.0, 0.0, 0.0, 0.0, 0.0, 0.0) * &scaling(0.5, 1.0, 1.0));
+
+    for y in 0..canvas_pixels {
+        let world_y = half - pixel_size * y as f64;
+        for x in 0..canvas_pixels {
+            let world_x = -half + pixel_size * x as f64;
+            let position = new_point(world_x, world_y, wall_z);
+            let r = Ray::new(ray_origin, (position - ray_origin).normalize());
+            let intersections = intersect_object(&shape, &r);
+
+            if intersections.hit().is_some() {
+                canvas.write_pixel(x, y, color);
+            }
+        }
     }
 
-    fs::write("canvas.ppm", c.to_ppm()).expect("Unable to write file");
+    fs::write("canvas.ppm", canvas.to_ppm()).expect("Unable to write file");
 }
